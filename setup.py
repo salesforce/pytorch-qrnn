bf4a7e5aa1d51505ec2c7ef76e4537a6b3f5597a
Matthew Honnibal <honnibal+gh@gmail.com>
2017-10-10 01:43:46 +0200

    Add cython to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
+from Cython.Build import cythonize
+import numpy
+import Cython.Compiler.Options
 from distutils.core import setup
+from distutils.extension import Extension
+Cython.Compiler.Options.cimport_from_pyx = True
+
+exts = [
+    Extension(name='torchqrnn._cpu_forget_mult',
+        sources=['torchqrnn/_cpu_forget_mult.pyx'],
+        language='c')
+]
 
 setup(
     name='PyTorch-QRNN',
@@ -6,4 +17,5 @@ setup(
     packages=['torchqrnn',],
     license='BSD 3-Clause License',
     long_description=open('README.md').read(),
+    ext_modules=cythonize(exts)
 )
