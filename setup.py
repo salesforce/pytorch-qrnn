b64688071e947451ac0dab2d1ac9ef73673406f8
Stephen Merity <smerity@smerity.com>
2017-10-23 12:37:30 -0700

    Update version to 0.2 for mult-GPU + fix long description bug

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PyTorch-QRNN',
-    version='0.1',
+    version='0.2',
     packages=['torchqrnn',],
-    license='BSD 3-Clause License',
-    long_description=open('README.md').read(),
+    license='BSD 3-Clause License'
 )
